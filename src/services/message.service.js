20a4fb52ab7b3e3a2188aef0539c2acd9c51693a
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2023-07-24 23:45:08 -0500

    implement authentication with external api integration

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -25,12 +25,13 @@ export const getPublicResource = async () => {
   }
 };
 
-export const getProtectedResource = async () => {
+export const getProtectedResource = async (accessToken) => {
   const config = {
     url: `${apiServerUrl}/api/messages/protected`,
     method: "GET",
     headers: {
       "content-type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
     },
   };
 
@@ -45,12 +46,13 @@ export const getProtectedResource = async () => {
   }
 };
 
-export const getAdminResource = async () => {
+export const getAdminResource = async (accessToken) => {
   const config = {
     url: `${apiServerUrl}/api/messages/admin`,
     method: "GET",
     headers: {
       "content-type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
     },
   };
 
